0a502b1297b6019f3dfb25cbfeccd7c2616f53d1
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-01-23 23:18:43 -0500

    completed mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,9 @@
            https://api.github.com/users/<your name>
 */
 
+const cardPromise = axios.get("https://api.github.com/users/kmilliner888");
+
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +27,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -46,6 +49,59 @@ const followersArray = [];
 
 */
 
+const githubCard = (cardObj) => {
+  const newCard = document.createElement('div');
+  const cardImage = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const cardName = document.createElement('h3');
+  const cardUserName = document.createElement('p');
+  const cardLocation = document.createElement('p');
+  const cardProfile = document.createElement('p');
+  const cardProfileLink = document.createElement('a');
+  const cardFollowers = document.createElement('p');
+  const cardFollowing = document.createElement('p');
+  const cardBio = document.createElement('p');
+
+  newCard.classList.add('card');
+  cardInfo.classList.add('card-info');
+  cardName.classList.add('name');
+  cardUserName.classList.add('username');
+
+  cardImage.src = cardObj.avatar_url;
+  cardName.textContent = cardObj.name;
+  cardUserName.textContent = cardObj.login;
+  cardLocation.textContent = `Location:   ${cardObj.location}`;
+  cardProfile.textContent = 'Profile:  ';
+  cardProfileLink.href = cardObj.html_url;
+  cardProfileLink.textContent = 'visit my Github page';
+  cardFollowers.textContent = `Followers:  ${cardObj.followers}`;
+  cardFollowing.textContent = `Following:  ${cardObj.following}`;
+  cardBio.textContent = `Bio:  ${cardObj.bio}`;
+
+  cardProfile.append(cardProfileLink);
+  cardInfo.append(cardName, cardUserName, cardLocation, cardProfile, cardFollowers, cardFollowing, cardBio);
+  newCard.append(cardImage, cardInfo);
+
+
+  return newCard;
+}
+
+
+const card = document.querySelector('.cards');
+
+cardPromise
+.then(response => {
+  console.log(response)
+  let newCard = githubCard(response.data)
+  card.append(newCard)
+})
+.catch(error => {
+  console.log('error!',error)
+})
+
+
+
+
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
@@ -53,3 +109,14 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+followersArray.forEach(follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+  .then(response => {
+    const newFollowerCard = githubCard(response.data)
+    card.append(newFollowerCard)
+  })
+  .catch(error => {
+    console.log('error!',error)
+  })
+})
